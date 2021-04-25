c23e5205e1ae891ddde794d3581b45ae04f6b916
Veeresh B V <Veereshbv04@gmail.com>
2021-04-25 16:49:44 +0530

    styling is pending

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 console.log("Script is working from a differsnt file");
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-div");
 
-function clickHandler(){
-    console.log("Button is clicked");
-    console.log("input",textInput.value)
+var serverURL = "https://api.funtranslations.com/translate/minion.json" ;
+
+function getserverURL(text){
+    return serverURL + "?" + "text=" + text ;
 }
 
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server,try again");
+}
 
+function clickHandler(){
+
+    var inputText = textInput.value;
+
+    fetch(getserverURL(inputText))
+    .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+
+}
 
 btnTranslate.addEventListener("click",clickHandler);
