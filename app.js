1732a7fa379661d58859d22d026650d7b86ffd8d
Veeresh B V <Veereshbv04@gmail.com>
2021-04-25 12:41:58 +0530

    pending

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("Hello World")
-alert("hhhh");
+var btnTransalate= document.querySelector("#btn-translate");
+var txtinput=document.querySelector("#txt-input");
+
+
+
+function clickHandler(){
+    console.log("clicked");
+
+}
+
+btnTransalate.addEventListener("click",clickHandler)
